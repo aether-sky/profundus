608bc5e9c052b33265695036e051174f2ef29847
sky <sky@abyss>
2024-10-06 09:28:21 -0400

    made a version that compiles and runs

---
+++ src/Game.scala
@@ -0,0 +1,9 @@
+package sky.aether
+
+import com.deweyvm.gleany.{GleanyGame, GleanyInitializer}
+
+class Game(initializer:GleanyInitializer) extends GleanyGame(initializer) {
+  //val sprite =
+  override def draw(): Unit = ()
+  override def update(): Unit = ()
+}
